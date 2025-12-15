4eb98843f09a8df6e8e9e413356aceccc85d2269
DeibyVargasChaves <dvargas30884@ufide.ac.cr>
2025-12-14 22:19:46 -0600

    Quitar panel inicio de paciente
    El panel no es necesario

--- src/main/java/com/proyecto/consultorioMedico/security/InicioSegunRol.java
+++ src/main/java/com/proyecto/consultorioMedico/security/InicioSegunRol.java
@@ -63,7 +63,7 @@ public class InicioSegunRol implements AuthenticationSuccessHandler {
             } else if (role.equals("ROLE_CLIENTE")) { 
                 Paciente paciente = pacienteService.getPacientePorIdUsuario(usuario.getIdUsuario());
                 if (paciente != null) {
-                    redirectUrl = "/paciente/inicio/" + paciente.getIdPaciente();
+                    redirectUrl = "/paciente/tratamientos/" + paciente.getIdPaciente();
                 } else {
                     System.out.println("Error, el suario no es valido" + username);
                     redirectUrl = "/";
